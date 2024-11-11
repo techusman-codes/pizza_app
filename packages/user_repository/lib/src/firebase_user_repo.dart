d315f1c66afd5e75473a614341e367dadfa3402a
Usman Umar Grba <Ugarba202@gmail.com>
2024-11-11 22:53:16 +0100

    first commit

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,6 +1,1 @@
-import 'package:user_repository/src/user_repo.dart';
 
-class FirebaseUserRepo implements UserRepository {
-  final FirebaseAuth _firebaseAuth;
-  final usersCollection = FirebaseFirestore.instance.collection('users');
-}
